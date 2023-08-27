0e28cf588d7aa52a36825d32b11509d8838a1131
Vitaly <98951280+Thechvi@users.noreply.github.com>
2023-08-27 18:54:00 +0300

    the highest salary

---
+++ Tasks/task Serg.js
@@ -0,0 +1,22 @@
+const arr = [
+	{ name: 'Ivan', age: 12, salary: 2500, position: 'middle' },
+	{ name: 'Vlad', age: 58, salary: 8500, position: 'lead' },
+	{ name: 'Vitalik', age: 38, salary: 500, position: 'junior-' }
+
+];
+
+// use 'forEach'
+const sumSalaries = (users) => {
+	let sum = 0;
+	users.forEach((el) => {
+		sum += el.salary;
+
+	});
+	return sum;
+};
+// use 'reduce'
+const sumSalaries = (users) => {
+	let sum = users.reduce((i, x) => i + x.salary, 0);
+	return sum;
+};
+console.log(sumSalaries(arr));
